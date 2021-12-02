6d9f486f0589e530829f157d951828c060aa57aa
Ta2ya-Ka10ri <95194691+Ta2ya-Ka10ri@users.noreply.github.com>
2021-12-02 23:23:53 +0900

    Add files via upload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,3 @@
 <?php
+require __DIR__ . '/_fixture/FunctionCallback.php';
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/TestCase.php';
-
-define('TEST_FILES_PATH', __DIR__ . '/_files/');
