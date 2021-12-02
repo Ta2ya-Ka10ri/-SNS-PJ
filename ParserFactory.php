2ec653115c574255d8db167110ea6e251081a4c8
Ta2ya-Ka10ri <95194691+Ta2ya-Ka10ri@users.noreply.github.com>
2021-12-02 23:30:19 +0900

    Add files via upload

--- ParserFactory.php
+++ ParserFactory.php
@@ -1,44 +1,91 @@
-<?php declare(strict_types=1);
+<?php
 
-namespace PhpParser;
+/*
+ * This file is part of Psy Shell.
+ *
+ * (c) 2012-2018 Justin Hileman
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+namespace Psy;
+
+use PhpParser\Lexer;
+use PhpParser\Parser;
+use PhpParser\ParserFactory as OriginalParserFactory;
+
+/**
+ * Parser factory to abstract over PHP parser library versions.
+ */
 class ParserFactory
 {
-    const PREFER_PHP7 = 1;
-    const PREFER_PHP5 = 2;
-    const ONLY_PHP7 = 3;
-    const ONLY_PHP5 = 4;
+    const ONLY_PHP5   = 'ONLY_PHP5';
+    const ONLY_PHP7   = 'ONLY_PHP7';
+    const PREFER_PHP5 = 'PREFER_PHP5';
+    const PREFER_PHP7 = 'PREFER_PHP7';
 
     /**
-     * Creates a Parser instance, according to the provided kind.
+     * Possible kinds of parsers for the factory, from PHP parser library.
      *
-     * @param int        $kind  One of ::PREFER_PHP7, ::PREFER_PHP5, ::ONLY_PHP7 or ::ONLY_PHP5
-     * @param Lexer|null $lexer Lexer to use. Defaults to emulative lexer when not specified
-     * @param array      $parserOptions Parser options. See ParserAbstract::__construct() argument
-     *
-     * @return Parser The parser instance
+     * @return array
      */
-    public function create(int $kind, Lexer $lexer = null, array $parserOptions = []) : Parser {
-        if (null === $lexer) {
-            $lexer = new Lexer\Emulative();
+    public static function getPossibleKinds()
+    {
+        return ['ONLY_PHP5', 'ONLY_PHP7', 'PREFER_PHP5', 'PREFER_PHP7'];
+    }
+
+    /**
+     * Is this parser factory supports kinds?
+     *
+     * PHP parser < 2.0 doesn't support kinds, >= 2.0 — does.
+     *
+     * @return bool
+     */
+    public function hasKindsSupport()
+    {
+        return \class_exists('PhpParser\ParserFactory');
+    }
+
+    /**
+     * Default kind (if supported, based on current interpreter's version).
+     *
+     * @return string|null
+     */
+    public function getDefaultKind()
+    {
+        if ($this->hasKindsSupport()) {
+            return \version_compare(PHP_VERSION, '7.0', '>=') ? static::ONLY_PHP7 : static::ONLY_PHP5;
         }
-        switch ($kind) {
-            case self::PREFER_PHP7:
-                return new Parser\Multiple([
-                    new Parser\Php7($lexer, $parserOptions), new Parser\Php5($lexer, $parserOptions)
-                ]);
-            case self::PREFER_PHP5:
-                return new Parser\Multiple([
-                    new Parser\Php5($lexer, $parserOptions), new Parser\Php7($lexer, $parserOptions)
-                ]);
-            case self::ONLY_PHP7:
-                return new Parser\Php7($lexer, $parserOptions);
-            case self::ONLY_PHP5:
-                return new Parser\Php5($lexer, $parserOptions);
-            default:
-                throw new \LogicException(
-                    'Kind must be one of ::PREFER_PHP7, ::PREFER_PHP5, ::ONLY_PHP7 or ::ONLY_PHP5'
-                );
+    }
+
+    /**
+     * New parser instance with given kind.
+     *
+     * @param string|null $kind One of class constants (only for PHP parser 2.0 and above)
+     *
+     * @return Parser
+     */
+    public function createParser($kind = null)
+    {
+        if ($this->hasKindsSupport()) {
+            $originalFactory = new OriginalParserFactory();
+
+            $kind = $kind ?: $this->getDefaultKind();
+
+            if (!\in_array($kind, static::getPossibleKinds())) {
+                throw new \InvalidArgumentException('Unknown parser kind');
+            }
+
+            $parser = $originalFactory->create(\constant('PhpParser\ParserFactory::' . $kind));
+        } else {
+            if ($kind !== null) {
+                throw new \InvalidArgumentException('Install PHP Parser v2.x to specify parser kind');
+            }
+
+            $parser = new Parser(new Lexer());
         }
+
+        return $parser;
     }
 }
--- Util/Json.php
+++ Util/Json.php
@@ -1,78 +1,33 @@
 <?php
+
 /*
- * This file is part of PHPUnit.
+ * This file is part of Psy Shell.
  *
- * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ * (c) 2012-2018 Justin Hileman
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace PHPUnit\Util;
+namespace Psy\Util;
 
-use PHPUnit\Framework\Exception;
-
+/**
+ * A static class to wrap JSON encoding/decoding with PsySH's default options.
+ */
 class Json
 {
     /**
-     * Prettify json string
+     * Encode a value as JSON.
      *
-     * @param string $json
+     * @param mixed $val
+     * @param int   $opt
      *
      * @return string
-     *
-     * @throws \PHPUnit\Framework\Exception
      */
-    public static function prettify(string $json)
+    public static function encode($val, $opt = 0)
     {
-        $decodedJson = \json_decode($json, true);
+        $opt |= JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
 
-        if (\json_last_error()) {
-            throw new Exception(
-                'Cannot prettify invalid json'
-            );
-        }
-
-        return \json_encode($decodedJson, JSON_PRETTY_PRINT);
-    }
-
-    /*
-     * To allow comparison of JSON strings, first process them into a consistent
-     * format so that they can be compared as strings.
-     * @return array ($error, $canonicalized_json)  The $error parameter is used
-     * to indicate an error decoding the json.  This is used to avoid ambiguity
-     * with JSON strings consisting entirely of 'null' or 'false'.
-     */
-    public static function canonicalize(string $json)
-    {
-        $decodedJson = \json_decode($json, true);
-
-        if (\json_last_error()) {
-            return [true, null];
-        }
-
-        self::recursiveSort($decodedJson);
-
-        $reencodedJson = \json_encode($decodedJson);
-
-        return [false, $reencodedJson];
-    }
-
-    /*
-     * JSON object keys are unordered while PHP array keys are ordered.
-     * Sort all array keys to ensure both the expected and actual values have
-     * their keys in the same order.
-     */
-    private static function recursiveSort(&$json)
-    {
-        if (false === \is_array($json)) {
-            return;
-        }
-
-        \ksort($json);
-
-        foreach ($json as $key => &$value) {
-            self::recursiveSort($value);
-        }
+        return \json_encode($val, $opt);
     }
 }
